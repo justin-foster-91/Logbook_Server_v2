e8b872469a37ee97de3577ab74652f40c223ea1c
justin-foster-91 <55926012+justin-foster-91@users.noreply.github.com>
2022-01-22 16:01:10 -0800

    Finished parsing of ship frames

--- processFrames.js
+++ processFrames.js
@@ -1,20 +1,16 @@
-// import { parse } from 'node-html-parser';
-let parse = require('node-html-parser').parse;
-// console.log(parse);
+const exp = require('constants');
 
 fs = require('fs')
 fs.readFile('tempShips.html', 'utf8', function (err, data) {
   if (err) {
     return console.log(err);
   }
-  // console.log(data);
   processData(data);
 });
 
-
 const processData = (data) => {
   let spanArray = [];
-  let shipJson = [];
+  let baseFrameJSON = [];
 
   let startSnip = data.search("<span")
   let endSnip = data.search("</span>") +7
@@ -42,11 +38,24 @@ const processData = (data) => {
     var [span, maximumCrew] = findComponent(span, '</b>', '<b>')
     var [span, cost] = findComponent(span, '</b>', '<h3')
     var [span, specialAbility] = findComponent(span, '</b>', '<h3')
-    if(dt === "ꝏ") dt = null;
-    if(expansions === "ꝏ") expansions = "0";
-    if(specialAbility === "0" || specialAbility === "") specialAbility = null;
 
-    shipJson.push({
+    if(dt === "—") dt = null;
+    if(expansions === "—") expansions = "0";
+    if(specialAbility === "0" || specialAbility === "") specialAbility = null;
+    mounts = parseMounts(mounts)
+    if(idx === 5) mounts.forwardArc.requirements = "1 light weapon must be a tracking weapon"
+
+    hp = parseHP(hp)
+    maneuverability = parseManeuverability(maneuverability)
+
+    if(typeof (dt) === 'string') dt = parseInt(dt)
+    ct = parseInt(ct)
+    if(typeof (expansions) === 'string' && expansions[0] !== "—") expansions = parseInt(expansions)
+    minimumCrew = parseInt(minimumCrew)
+    maximumCrew = parseInt(maximumCrew)
+    cost = parseInt(cost)
+
+    baseFrameJSON.push({
       "type": type,
       "sourceLink": sourceLink,
       "sourceText": sourceText,
@@ -64,12 +73,10 @@ const processData = (data) => {
     })
   })
   
-    //TODO: mounts and expansions into object
-    //TODO: parse numbers
 
-
-  console.log(shipJson[0]);
-  // console.log(spanArray[0]);
+  // console.log(baseFrameJSON[9]);
+  console.log(JSON.stringify(baseFrameJSON))
+  // return baseFrameJSON;
 }
 
 // [span, type]
@@ -83,7 +90,7 @@ const findComponent = (span, removeString, endString) => {
 
   component = component
     .replace("<br />", "").replace("\r\n", "")
-    .replace("&mdash;", "ꝏ").replace(";", "")
+    .replace("&mdash;", "—").replace(";", "")
     .replace("<br />", "").replace("</span", "").replace("br />", "")
     .replace("unlimited", "unlimited;")
     .trim()
@@ -91,5 +98,96 @@ const findComponent = (span, removeString, endString) => {
   return [span, component];
 }
 
+const parseMounts = (mounts) => {
+  let arcSearches = ["forward arc", "port arc", "starboard arc", "aft arc", "turret"]
+  let returnMounts = {}
 
+  for(let arc of arcSearches){
+    let snipLength = arc.length
+    let snipPoint = mounts.search(arc)
+
+    if(snipPoint >= 0){
+      mounts = mounts.slice(snipPoint + snipLength)
+
+      let start = mounts.search(/[(]/)
+      let end = mounts.search(/[)]/)
+
+      let weaponLayout = mounts.slice(start+1, end).split(",")
+      let weaponTypes = ["light", "heavy", "capital", "spinal"]
+
+      // [ '1 capital', ' 2 heavy' ] => [ [ 'capital', 1 ], [ 'heavy', 2 ] ],
+      weaponLayout = weaponLayout.map(weaponArc => {
+        let weaponArcObject = {}
+
+        for(let type of weaponTypes){
+          if(weaponArc.search(type) >= 0){
+            weaponArcObject = [type, parseInt(weaponArc)]
+          }
+        }
+
+        return weaponArcObject
+      })
+
+      // [ [ 'capital', 1 ], [ 'heavy', 2 ] ] => { { 'capital': 1 }, { 'heavy': 2 } }
+      weaponLayout = Object.fromEntries(weaponLayout)
+
+      if(arc === "forward arc") returnMounts.forwardArc = weaponLayout
+      if(arc === "port arc") returnMounts.portArc = weaponLayout
+      if(arc === "starboard arc") returnMounts.starboardArc = weaponLayout
+      if(arc === "aft arc") returnMounts.aftArc = weaponLayout
+      if(arc === "turret") returnMounts.turret = weaponLayout
+    }
+  }
+
+  return returnMounts
+}
+
+const parseHP = (hp) => {
+  let newHP = {}
+
+  let start = hp.search(/[(]/)
+  let end = hp.search(/[)]/)
+
+  newHP.increment = parseInt(hp.slice(start+10, end))
+
+  hp = hp.slice(0, start-1)
+  newHP.startTotal = parseInt(hp)
+
+  return newHP
+}
+
+const parseManeuverability = (maneuverability) => {
+  let moveTypes = ["Clumsy", "Poor", "Average", "Good", "Perfect"]
+  let parsedMove = ''
+
+  for(let type of moveTypes){
+    if(maneuverability.search(type.toLowerCase()) >= 0) parsedMove = type
+  }
+
+  return parsedMove
+}
   
+{/* 
+  maneuverability: {
+    clumsy: {
+      pilotingMod: -2,
+      turn: 4
+    },
+    poor: {
+      pilotingMod: -1,
+      turn: 3
+    },
+    average: {
+      pilotingMod: 0,
+      turn: 2
+    },
+    good: {
+      pilotingMod: 1,
+      turn: 1
+    },
+    perfect: {
+      pilotingMod: 2,
+      turn: 0
+    }
+  }
+*/}
